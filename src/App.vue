dd392f9f6d811c8d87226be450d80e11ead4b089
Mart van der Zalm <martvanderzalm@gmail.com>
2024-01-02 00:13:23 +0100

    App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,21 @@
 <template>
-  <PieChart/>
+
+  <PieChart
+    name="Brands"
+    :width="350"
+    :height="350"
+    :outerRadius="140"
+    :innerRadius="70"
+    :data="slices"
+    />
+
+  <PieChart
+    :width="350"
+    :height="350"
+    :outerRadius="140"
+    :innerRadius="70"
+    />
+
 </template>
 
 <script>
@@ -9,7 +25,21 @@ export default {
   name: 'App',
   components: {
     PieChart
-  }
+  },
+  data() {
+    return {
+      slices: {
+        '2024-01-01': 324546,
+        '2023-12-18': 435264,
+        '2023-12-29': 413524,
+        '2023-12-12': 134524,
+        '2023-12-17': 124524,
+        '2023-12-23': 334524,
+        '2023-12-16': 354524,
+        '2023-12-14': 294524,
+      },
+    };
+  },
 }
 </script>
 
